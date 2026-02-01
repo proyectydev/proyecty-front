dd8cfe3adfa78308b39860f2fec442b9ec7873b4
MacoSuarezSB <marco.suarez@segurosbolivar.com>
2026-01-31 20:09:42 -0500

    Ajuste logo en pagina usuario

--- src/pages/user/MyAccountPage.tsx
+++ src/pages/user/MyAccountPage.tsx
@@ -34,7 +34,7 @@ export function MyAccountPage() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             <img 
-              src="/assets/Logo-Proyecty-Colombia_FB.png" 
+              src="/assets/Logo-Proyecty-Colombia_FB1.png" 
               alt="Proyecty" 
               className="h-10 object-contain"
             />
